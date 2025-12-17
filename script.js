ecced4fdad8a916e2c0d3c0dba262e5fa2eb4c16
satyabratarajat-cyber <satyabratarajat@gmail.com>
2025-12-17 17:29:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,11 @@
-function saveDaily(){
-  const v = dailyInput.value.trim();
-  if(!v) return alert('Daily empty');
-
-  fetch('update.php',{
-    method:'POST',
-    headers:{'Content-Type':'application/json'},
-    body:JSON.stringify({
-      password:"damodar@123",
-      daily:v
-    })
-  })
-  .then(r=>r.json())
-  .then(res=>{
-    if(res.status==="success"){
-      dailyOut.value="SAVED ON SERVER: "+v;
-    }else{
-      alert("Server error");
-    }
-  });
+function loadResult(){
+  fetch("data.json")
+    .then(res => res.json())
+    .then(data => {
+      document.getElementById("mainResult").innerText = data.result;
+      document.getElementById("updateTime").innerText =
+        "Updated: " + data.updated_at;
+    });
 }
+
+loadResult();
